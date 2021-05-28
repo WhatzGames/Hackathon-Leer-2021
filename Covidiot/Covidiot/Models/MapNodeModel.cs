fa271b7cd95267dd8745b87c4980ee7b3a22adf2
t.stamma <t.stamma@journaway.com>
2021-05-28 17:36:46 +0200

    added jsonpropertynames in MapNodeModel.cs

--- Covidiot/Covidiot/Models/MapNodeModel.cs
+++ Covidiot/Covidiot/Models/MapNodeModel.cs
@@ -1,4 +1,6 @@
-﻿namespace Covidiot.Models
+﻿using System.Text.Json.Serialization;
+
+namespace Covidiot.Models
 {
     public class MapNodeModel
     {
@@ -7,33 +9,66 @@
 
     public class TimedNodeAction
     {
+        [JsonPropertyName("description")]
         public string Description { get; set; }
+        
+        [JsonPropertyName("timeOfDayStart")]
         public int TimeOfDayStart { get; set; }
+        
+        [JsonPropertyName("timeOfDayEnd")]
         public int TimeOfDayEnd { get; set; }
         
-        public Action Action1  { get; set; }
-        public Action Action2  { get; set; }
-        public Action Action3  { get; set; }
 
+        [JsonPropertyName("action1")]
+        public Action Action1  { get; set; }
+        
+        [JsonPropertyName("action2")]
+        public Action Action2  { get; set; }
+        
+        [JsonPropertyName("action3")]
+        public Action Action3  { get; set; }
+        
+        
+        [JsonPropertyName("here")]
         public MapNodeCoordinate Here { get; set; }
+        
+        [JsonPropertyName("north")]
         public MapNodeCoordinate North { get; set; }
+
+        [JsonPropertyName("east")]
         public MapNodeCoordinate East { get; set; }
+        
+        [JsonPropertyName("south")]
         public MapNodeCoordinate South { get; set; }
+        
+        [JsonPropertyName("west")]
         public MapNodeCoordinate West { get; set; }
     }
 
     public class Action
     {
+        [JsonPropertyName("text")]
         public string Text { get; set; }
+        
+        [JsonPropertyName("probability")]
         public decimal Probability { get; set; }
+        
+        [JsonPropertyName("score")]
         public int Score { get; set; }
+        
+        [JsonPropertyName("response")]
         public string Response { get; set; }
+        
+        [JsonPropertyName("newStart")]
         public MapNodeCoordinate NewStart { get; set; }
     }
 
     public class MapNodeCoordinate
     {
+        [JsonPropertyName("xCoordinate")]
         public int XCoordinate { get; set; }
+        
+        [JsonPropertyName("yCoordinate")]
         public char YCoordinate { get; set; }
     }
 }
