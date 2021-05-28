f39216dd76a44e989087f792fc3da6edc55c4f42
t.stamma <t.stamma@journaway.com>
2021-05-28 17:18:45 +0200

    Created MapNodeModel.cs

---
+++ Covidiot/Covidiot/Models/MapNodeModel.cs
@@ -0,0 +1,34 @@
+﻿namespace Covidiot.Models
+{
+    public class MapNodeModel
+    {
+        public string Description { get; set; }
+        public int TimeOfDayStart { get; set; }
+        public int TimeOfDayEnd { get; set; }
+        
+        public Action Action1  { get; set; }
+        public Action Action2  { get; set; }
+        public Action Action3  { get; set; }
+
+        public MapNodeCoordinate Here { get; set; }
+        public MapNodeCoordinate North { get; set; }
+        public MapNodeCoordinate East { get; set; }
+        public MapNodeCoordinate South { get; set; }
+        public MapNodeCoordinate West { get; set; }
+    }
+
+    public class Action
+    {
+        public string Text { get; set; }
+        public decimal Probability { get; set; }
+        public int Score { get; set; }
+        public string Response { get; set; }
+        public MapNodeCoordinate NewStart { get; set; }
+    }
+
+    public class MapNodeCoordinate
+    {
+        public int XCoordinate { get; set; }
+        public char YCoordinate { get; set; }
+    }
+}
